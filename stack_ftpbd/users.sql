dde6aeb5d28ab1a0a4cce3f19e840fcef5afbe72
raulmoes <raulillomoesp21@gmail.com>
2022-11-22 23:18:35 +0100

    Proyecto

---
+++ stack_ftpbd/users.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users (
+  User VARCHAR(255) BINARY NOT NULL,
+  Password VARCHAR(255) BINARY NOT NULL,
+  Uid INT NOT NULL default '-1',
+  Gid INT NOT NULL default '-1',
+  Dir VARCHAR(255) BINARY NOT NULL,
+  PRIMARY KEY (User)
+);
